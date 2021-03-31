09bbba9b40284ca7fbef8b75f61b7adefcfc033d
chryl <cyl5yy412@gmail.com>
2021-03-31 14:04:56 +0800

    redis

--- src/test/java/com/chryl/ChrRedisApplicationTests.java
+++ src/test/java/com/chryl/ChrRedisApplicationTests.java
@@ -9,8 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -53,4 +52,61 @@ public class ChrRedisApplicationTests {
         return "sys_test:" + configKey;
     }
 
+
+    //========================测试redis数据类型========================
+//    List<String> strRed = Arrays.asList("1991", "1992", "1993", "1994", "1995", "1996");
+    @Test
+    public void contextLoads2() {//list
+        List<String> strRed = Arrays.asList("1991", "1992", "1993", "1994", "1995", "1996", "1991", "1992");
+        long strRed1 = SpringUtils.getBean(RedisCache.class).setCacheList("strRed", strRed);
+        System.out.println(strRed1);
+
+        List<String> strRed2 = SpringUtils.getBean(RedisCache.class).getCacheList("strRed");
+        System.out.println(strRed2);
+    }
+
+    /*
+    @Test
+    public void contextLoads3() {
+//        List<Integer> strRed = Arrays.asList(1991, 1992, 1993, 1994, 1995, 1996, 1991, 1992);
+//        Set<Integer> hashSet = new HashSet<>(strRed);
+
+        List<String> strRed = Arrays.asList("1991", "1992", "1993", "1994", "1995", "1996", "1991", "1992");
+        Set<String> hashSet = new HashSet<>(strRed);
+
+        System.out.println(hashSet);
+        long strRed1 = SpringUtils.getBean(RedisCache.class).setCacheSet("strRedSet", hashSet);
+        System.out.println(strRed1);
+    Set<String> strRedSet = redisCache.getCacheSet("strRedSet");
+
+    }
+    */
+
+    @Test
+    public void contextLoads4() {//map
+
+        Map<String, Integer> a = new HashMap<>();
+        a.put("zz", 10992);
+        a.put("kk2", 1992);
+        a.put("z2z", 192);
+        a.put("z4z", 109);
+        a.put("z1z", 192);
+        redisCache.setCacheMap("redMap", a);
+
+
+        Map<String, Object> redMap = redisCache.getCacheMap("redMap");
+        System.out.println(redMap);
+    }
+
+
+    @Test
+    public void contextLoads5() {//hash
+
+        List<Integer> strRed = Arrays.asList(1991, 1992, 1993, 2);
+        redisCache.setCacheMapValue("redStrKey", "strHashKey", strRed);
+        redisCache.setCacheMapValue("redStrKey", "strHashKey2", strRed);
+
+        List<Integer> strRed2 = redisCache.getCacheMapValue("redStrKey", "strHashKey");
+        System.out.println(strRed2);
+    }
 }
