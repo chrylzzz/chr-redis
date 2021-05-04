ccc9551b8d3229ea26dde55b5f5ccc0019be31eb
chryl <cyl5yy412@gmail.com>
2021-05-04 18:07:05 +0800

    redisson分布式锁学习

---
+++ src/main/java/com/chryl/controller/redisson/RedissonConfig.java
@@ -0,0 +1,27 @@
+package com.chryl.controller.redisson;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Chr.yl on 2021/5/3.
+ *
+ * @author Chr.yl
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson() {
+        Config config = new Config();
+
+//        config.useClusterServers().addNodeAddress("127.0.0.1:6379");
+//        config.useSingleServer().setAddress("rediss://127.0.0.1:6379");//安全连接
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+
+        return Redisson.create(config);
+    }
+}
